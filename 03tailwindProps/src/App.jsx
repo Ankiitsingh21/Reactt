5c9fb810baca5b0d22fe67627134857fca8ab5af
ankit singh <ankiitsingh21@gmail.com>
2025-07-21 20:29:00 +0530

    Completed the tailwind and props

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl' >Tail wind Test</h1>
-      <Card channel="Hello world" someObj={newArr}/>
+      <Card channel="Hello world" someObj={newArr  }/>
       <Card/>
       <Card/>
     </>
